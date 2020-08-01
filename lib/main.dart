427d7dfd4a4a644053e14578ab887e1425d4f65c
orangeztorm <taiwokenny45@gmail.com>
2020-08-01 12:12:31 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          ClipPath(
+            child: Container(
+              color: Colors.black.withOpacity(0.8),
+            ),
+            clipper: getClipper(),
+          ),
+          Positioned(
+            width: 350.0,
+            left: 25.0,
+            top: MediaQuery.of(context).size.height / 5,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                      boxShadow: [
+                        BoxShadow(blurRadius: 7.0, color: Colors.black)
+                      ]),
+                ),
+                SizedBox(height: 50.0),
+                Text('MR FREEMAN',
+                    style: TextStyle(
+                      fontFamily: 'MonteSerrat',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text('Subscribe',
+                    style: TextStyle(
+                      fontFamily: 'MonteSerrat',
+                      fontSize: 17.0,
+                      fontStyle: FontStyle.italic,
+                    )),
+                SizedBox(height: 25.0,),
+                Container(
+                  height: 30.0,
+                  width: 95.0,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class getClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+    path.lineTo(0.0, size.height / 1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+}
